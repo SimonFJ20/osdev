76036538518f5d0af9d148a6520199ef5968e80b
SimonFJ20 <simonfromjakobsen@gmail.com>
2022-02-25 01:15:11 +0100

    setup file confirmation

--- setup-i686.sh
+++ setup-i686.sh
@@ -1,9 +1,30 @@
 #!/bin/sh
 
+echo "GNU GCC and GNU Binutils will be installed into in $HOME/osdev/cross/"
+echo "This script should be run from inside the $HOME/osdev/ folder"
+echo "Make is set to use 16 CPU threads, if it shouldn't, change it"
+echo ""
+read -p "Do you want to continue? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+fi
+
+
+
 mkdir -p $HOME/osdev/src
 cd $HOME/osdev/src
-#wget "https://ftp.gnu.org/gnu/binutils/binutils-2.38.tar.gz" 
-#wget "https://ftp.gnu.org/gnu/gcc/gcc-11.2.0/gcc-11.2.0.tar.gz"
+
+read -p "Should new sourcecode be downloaded? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    wget "https://ftp.gnu.org/gnu/binutils/binutils-2.38.tar.gz"
+    wget "https://ftp.gnu.org/gnu/gcc/gcc-11.2.0/gcc-11.2.0.tar.gz"
+    tar -xf "binutils-2.38.tar.gz"
+    tar -xf "gcc-11.2.0.tar.gz"
+fi
 
 export PREFIX="$HOME/osdev/cross"
 export TARGET=i686-elf
